a5de7c25bf69f30c11ef933df4bb54619173bbd9
Aditya <sharmaditya5150@gmail.com>
2024-04-16 14:32:33 +0530

    Add to cart functionality : part 1

--- GoFood/src/App.jsx
+++ GoFood/src/App.jsx
@@ -14,19 +14,22 @@ import Login from './screens/Login'
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import SignUp from './screens/SignUp'
+import { CartProvider } from './components/ContextReducer'
 
 function App() {
 
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Home/>} />
-          <Route exact path="/login" element={<Login />} /> 
-          <Route exact path="/createuser" element={<SignUp />} /> 
-        </Routes>
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="/login" element={<Login />} /> 
+            <Route exact path="/createuser" element={<SignUp />} /> 
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   )
 }
 
