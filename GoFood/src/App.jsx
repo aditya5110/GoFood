18a69891eccaef94cab9021e358e737746dc5b66
Aditya <sharmaditya5150@gmail.com>
2024-05-07 19:54:08 +0530

    displaying  the My Orders Page

--- GoFood/src/App.jsx
+++ GoFood/src/App.jsx
@@ -15,6 +15,7 @@ import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import SignUp from './screens/SignUp'
 import { CartProvider } from './components/ContextReducer'
+import MyOrder from './screens/MyOrder'
 
 function App() {
 
@@ -25,7 +26,9 @@ function App() {
           <Routes>
             <Route exact path="/" element={<Home/>} />
             <Route exact path="/login" element={<Login />} /> 
-            <Route exact path="/createuser" element={<SignUp />} /> 
+            <Route exact path="/createuser" element={<SignUp />} />
+            <Route exact path="/myOrder" element={<MyOrder />} />
+
           </Routes>
         </div>
       </Router>
